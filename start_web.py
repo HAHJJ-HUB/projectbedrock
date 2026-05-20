d0e843a4aed881b8b8113db262c599510065169b
HAHJJ-HUB <humanashell.jj@gmail.com>
2026-05-20 13:59:03 +1000

    Revert "Add CrewAI-powered intelligence research system with web dashboard"

--- start_web.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""Start the case file web dashboard."""
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run("web.app:app", host="0.0.0.0", port=8000, reload=True)
